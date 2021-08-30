41d67a7a08cb17a0af928d5c95d05ead2b0dd8d0
Tonderai Muchada <tonderaim@afrosoft.co.zw>
2021-08-30 21:46:58 +0200

    first commit

---
+++ SubCategoryRepository.java
@@ -0,0 +1,12 @@
+/*
+ * Tonderai Muchada
+ * Copyright (c) 31/08/2020, 03:30.
+ */
+
+package zw.co.murambindahub;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import subcategory.SubCategory;
+
+public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
+}
